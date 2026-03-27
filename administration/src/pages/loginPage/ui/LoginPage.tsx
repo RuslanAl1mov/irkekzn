c8b1ce47027ad9d9f4fecf4bba84ddffecd641a1
Ruslan Alimov <ruslan.alimov@adskill.com>
2026-03-27 15:03:47 +0300

    Добавил переключение фона на странице логина в зависимости от выбранной темы

--- administration/src/pages/loginPage/ui/LoginPage.tsx
+++ administration/src/pages/loginPage/ui/LoginPage.tsx
@@ -1,12 +1,21 @@
-import { LoginForm } from "@/features/auth/login";
 import cls from "./LoginPage.module.css";
 import cn from "classnames";
 
+import { useThemeState } from "@/app/providers/theme/model/store";
+import { LoginForm } from "@/features/auth/login";
+
+import backgroundImageLight from "@/assets/backgrounds/login-white-bg.jpg";
+import backgroundImageDark from "@/assets/backgrounds/login-black-bg.png";
+
 
 export const LoginPage = () => {
+  const { theme } = useThemeState();
+
   return (
     <main className={cn("main", cls.main)}>
-      <div className={cn("mainBlock", cls.mainBlock)}>
+      <div className={cn("mainBlock", cls.mainBlock)}
+        style={{ backgroundImage: `url(${theme === "light" ? backgroundImageLight : backgroundImageDark})` }}
+      >
         <LoginForm />
       </div>
     </main>
